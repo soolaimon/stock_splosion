19b37c0b745577697a404eb24d0ac975e8de0e2d
Dave Nelson <davidicus21@gmail.com>
2016-01-18 20:24:27 -0700

    company model

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require './lib/stock_splosion'
+require './models/company'
+
 include StockSplosion
 
 get '/' do
+  @companies = read_list.map {|c| Company.new(c) }
   erb :index
 end
