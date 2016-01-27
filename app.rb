2609cd1cac03da044b6d104cdd91c74740cc166c
Dave Nelson <davidicus21@gmail.com>
2016-01-26 18:30:34 -0700

    search endpoint

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ require './models/company'
 
 
 get '/' do
-  @companies = Company.all.map {|c| Company.new(c) }
   erb :index
+end
+
+
+get '/search' do
+  query = params[:query]
+  companies = Company.all.map {|c| Company.new(c) }
+  companies = companies.select {|c| c.symbol.include? query}
+  json companies
 end
